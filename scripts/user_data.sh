7324b4c8eb99752f03ca19fb7afc0a83ed9af89a
arikaranredblue-hue <arikaranredblue@gmail.com>
2026-03-30 18:04:26 +0530

    updated-code

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -10,11 +10,31 @@ yum install docker -y
 systemctl start docker
 systemctl enable docker
 
-# Add ec2-user to docker group
-usermod -aG docker ec2-user
-
-# Pull your image
+# Run first container
 docker pull arikaran0007/nginx-app
+docker run -d -p 80:80 --name myapp arikaran0007/nginx-app
 
-# Run container
-docker run -d -p 80:80 arikaran0007/nginx-app
+# Create auto-update script
+cat <<EOF > /home/ec2-user/update.sh
+#!/bin/bash
+
+IMAGE="arikaran0007/nginx-app"
+
+docker pull $IMAGE
+
+CONTAINER_ID=\$(docker ps -q --filter ancestor=\$IMAGE)
+
+if [ -z "\$CONTAINER_ID" ]; then
+  docker run -d -p 80:80 --name myapp \$IMAGE
+else
+  docker stop \$CONTAINER_ID
+  docker rm \$CONTAINER_ID
+  docker run -d -p 80:80 --name myapp \$IMAGE
+fi
+EOF
+
+# Give permission
+chmod +x /home/ec2-user/update.sh
+
+# Run every 1 minute
+echo "* * * * * /home/ec2-user/update.sh" >> /var/spool/cron/ec2-user
